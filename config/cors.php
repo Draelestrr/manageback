ce25ce95a7e56c86cf53f612cb3537aac0f9e617
Draelestrr <andres.blanco02@inacapmail.cl>
2024-11-29 10:45:36 -0300

    correccion

--- config/cors.php
+++ config/cors.php
@@ -1,34 +1,23 @@
 <?php
 
-return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
+    return [
 
-    'paths' => ['*'],
+        'paths' => ['api/*', 'sanctum/csrf-cookie'], // Define las rutas que aceptarán solicitudes CORS
+    
+        'allowed_methods' => ['*'], // Métodos HTTP permitidos ('GET', 'POST', etc. o '*')
+    
+        'allowed_origins' => ['http://localhost:5173'], // Orígenes permitidos (dominios o '*')
+    
+        'allowed_origins_patterns' => [], // Patrones de origen permitidos (si usas expresiones regulares)
+    
+        'allowed_headers' => ['*'], // Headers permitidos en las solicitudes
+    
+        'exposed_headers' => [], // Headers visibles para el frontend
+    
+        'max_age' => 0, // Tiempo de caché para preflight (opcional)
+    
+        'supports_credentials' => true, // Habilitar cookies y credenciales en las solicitudes
+    ];
+    
 
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => true,
-
-];
